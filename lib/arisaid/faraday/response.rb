f2ad259c7ee80111a7fae5581e46488aa43d73ca
linyows <linyows@gmail.com>
2015-12-18 15:36:14 +0900

    support debug

--- lib/arisaid/faraday/response.rb
+++ lib/arisaid/faraday/response.rb
@@ -2,11 +2,22 @@ module Arisaid
   module Faraday
     class Response < ::Faraday::Middleware
       def call(env)
-        @app.call(env).on_complete do |v|
-          if Arisaid.debug? && defined?(AwesomePrint)
-            require 'awesome_print'
-            ap v.response.to_hash.slice(*valid_debug_attributes)
+        @app.call(env).on_complete do |_env|
+          show_response(_env) if Arisaid.debug? && defined?(AwesomePrint)
+        end
+      end
+
+      def show_response(env)
+        require 'awesome_print'
+
+        env.response.to_hash.slice(*valid_debug_attributes).each do |k, v|
+          if k == :body
+            k = :response_body
+            v = Sawyer::Agent.serializer.decode(v)
           end
+
+          puts "#{k}:"
+          ap v
         end
       end
 
