ee69a0d0ccad9d198862d791012d1d9763441714
linyows <linyows@gmail.com>
2015-12-19 20:14:45 +0900

    fix cli

--- lib/arisaid/cli.rb
+++ lib/arisaid/cli.rb
@@ -3,10 +3,15 @@ require 'thor'
 
 module Arisaid
   class CLI < Thor
+    RESOURCES = %w(
+        users
+        usergroups
+      )
+
     option :team, type: :string, aliases: '-t', desc: 'slack team'
     option :token, type: :string, aliases: '-p', desc: 'slack token'
     option :debug, type: :boolean, aliases: '-d', desc: 'debug'
-    desc 'show [resource]', 'show [users, usergroups]'
+    desc 'show [RESOURCE]', "show #{RESOURCES.join(', ')}"
     def show(resource)
       Arisaid.debug = true if options[:debug]
       Arisaid.send(:"#{resource}", options[:team]).show
@@ -15,7 +20,7 @@ module Arisaid
     option :team, type: :string, aliases: '-t', desc: 'slack team'
     option :token, type: :string, aliases: '-p', desc: 'slack token'
     option :debug, type: :boolean, aliases: '-d', desc: 'debug'
-    desc 'show [resource]', 'show [users, usergroups]'
+    desc 'save [RESOURCE]', "save #{RESOURCES.join(', ')}"
     def save(resource)
       Arisaid.debug = true if options[:debug]
       Arisaid.send(:"#{resource}", options[:team]).save
@@ -25,7 +30,7 @@ module Arisaid
     option :token, type: :string, aliases: '-p', desc: 'slack token'
     option :debug, type: :boolean, aliases: '-d', desc: 'debug'
     option :dryrun, type: :boolean, aliases: '-n', desc: 'dry run'
-    desc 'show [resource]', 'show [users, usergroups]'
+    desc 'apply [RESOURCE]', "apply #{RESOURCES.join(', ')}"
     def apply(resource)
       Arisaid.read_only = true if options[:dryrun]
       Arisaid.debug = true if options[:debug]
