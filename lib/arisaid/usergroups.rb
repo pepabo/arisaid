9f8dcf1440c92e2a9924bdc144a73a6281617093
pyama86 <pyama@pepabo.com>
2020-01-22 17:29:23 +0900

    use compacat

--- lib/arisaid/usergroups.rb
+++ lib/arisaid/usergroups.rb
@@ -66,8 +66,8 @@ module Arisaid
           end
 
           if users_changed?(src, dst)
-            add = src['users'].flatten.sort  - dst['users'].flatten.sort
-            delete = dst['users'].flatten.sort - src['users'].flatten.sort
+            add = src['users'].flatten.compact.sort  - dst['users'].flatten.compact.sort
+            delete = dst['users'].flatten.compact.sort - src['users'].flatten.compact.sort
             add.each do |u|
               puts "  + user #{u}"
             end
@@ -107,7 +107,7 @@ module Arisaid
       src['name'] == dst['name'] &&
           src['description'] == dst['description'] &&
           src['handle'] == dst['handle'] &&
-          src['users'].flatten.sort == dst['users'].flatten.sort
+          src['users'].flatten.compact.sort == dst['users'].flatten.compact.sort
     end
 
     def changed?(src, dst)
@@ -115,7 +115,7 @@ module Arisaid
     end
 
     def users_changed?(src, dst)
-      src['users'].flatten.sort != dst['users'].flatten.sort
+      src['users'].flatten.compact.sort != dst['users'].flatten.compact.sort
     end
 
     def description_changed?(src, dst)
