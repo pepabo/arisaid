f1db8ecf2b057eed76f562012ddeeb1a286cef44
Yuki Koya <ykky@pepabo.com>
2022-06-22 16:52:15 +0900

    Raise when users is empty

--- test/arisaid/syncable_test.rb
+++ test/arisaid/syncable_test.rb
@@ -23,4 +23,22 @@ YML
     assert_equal @dummy.local[1]["users"], %w(foobar1 foobar2 foobar3)
     File.delete 'object.yml'
   end
+
+  def test_local_when_empty_users
+    @dummy = Object.new
+    @dummy.extend(Arisaid::Syncable)
+
+    File.write 'object.yml', <<-YML.lstrip
+---
+- name: tokyo
+  description: Tokyo Members
+  handle: tokyo
+  users:
+YML
+    e = assert_raises Arisaid::InvalidConf do
+      @dummy.local
+    end
+    assert_equal "Empty 'users' are not allowed: tokyo", e.message
+    File.delete 'object.yml'
+  end
 end
